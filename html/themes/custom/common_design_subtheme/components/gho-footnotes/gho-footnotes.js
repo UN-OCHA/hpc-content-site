f6a435dcc76caaad978388da95d4d15eb9ea6745
orakili <github@orakili.net>
2020-11-12 17:44:43 +0900

    [GHO-81] Add support for older browsers for the footnotes popup

--- html/themes/custom/common_design_subtheme/components/gho-footnotes/gho-footnotes.js
+++ html/themes/custom/common_design_subtheme/components/gho-footnotes/gho-footnotes.js
@@ -94,19 +94,102 @@
     intersectionObserver.observe(list.parentNode);
   }
 
-  // Prevent the footnotes from being processed several times.
+  /**
+   * Update the references visibility when scrolling.
+   *
+   * This simulates the same behavior as the intersection observers above:
+   *
+   * When a reference is in the top half of the viewport then the cooresponding
+   * footnote is displayed in a popup at the bottom of the screen.
+   *
+   * When the reference list appears at the bottom of the screen, a flag is set
+   * to disable the popup behavior.
+   */
+  function handleScroll() {
+    var viewportHeight = document.documentElement.clientHeight;
+
+    // Disable the popup behavior if the reference list is visible.
+    // The 0.8 is to have the same behavior as the marginRoot for the list
+    // intersection observer.
+    if (list.parentNode.getBoundingClientRect().top < 0.8 * viewportHeight) {
+      list.setAttribute('data-no-popup', '');
+    }
+    // Otherwise update the visibility of all the references.
+    else {
+      // The half height is to have similar behavior to the intersection
+      // observer marginRoot.
+      var minY = 0;
+      var maxY = viewportHeight / 2;
+
+      // Update the visibility of the references.
+      //
+      // Note we update the footnotes in a separate loop than the one to get
+      // the positions of the references to improve performance because the
+      // the positions are recalculated after updating the DOM. Grouping the
+      // read and write improves performance in older browsers.
+      var data = getReferenceData();
+      for (var i = 0, l = data.length; i < l; i++) {
+        var item = data[i];
+        var posY = item[1];
+        setFootnoteVisibility(item[0], minY < posY && posY < maxY);
+      }
+
+      list.removeAttribute('data-no-popup');
+    }
+
+    updateFootnoteList(list);
+
+    // Reset to allow another call to this function next time the window is
+    // scrolled.
+    scheduled = false;
+  }
+
+  /**
+   * Get the id and position of the references.
+   */
+  function getReferenceData() {
+    var data = [];
+    for (var i = 0, l = references.length; i < l; i++) {
+      var reference = references[i];
+      var rect = reference.getBoundingClientRect();
+      // The half height is to have similar behavior to the intersection
+      // observer threshold.
+      data.push([reference.hash.substr(1), rect.top + (rect.height / 2)]);
+    }
+    return data;
+  }
+
+  // Prevent the footnotes from being processed several times and skip if
+  // there are is footnote list to process.
   var list = document.querySelector('.gho-footnote-list--accumulated .gho-footnote-list__wrapper');
   if (!list || list.hasAttribute('data-footnotes-processed')) {
     return;
   }
   list.setAttribute('data-footnotes-processed', '');
 
-
   // Observe the footnote references entering and leaving the viewport
   // and display them in a popup at the bottom of the screen.
   if ('IntersectionObserver' in window) {
     observeReferences(list);
     observeList(list);
+  }
+  // Default to a scroll event listener for browser not supporting observers.
+  else if ('addEventListener' in window) {
+    // Store the references so that we don't need to run the queryselector
+    // everytime we compute their positions.
+    var references = document.querySelectorAll('.gho-footnote-reference a');
+
+    // Note the use of requestAnimationFrame may not be necessary as the scroll
+    // event is fired at about the same rate according to MDN.
+    // @see https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
+    var scheduled = false;
+    window.addEventListener('scroll', function (event) {
+      // This prevent excessive computation and repainting.
+      if (!scheduled) {
+        scheduled = true;
+        requestAnimationFrame(handleScroll);
+      }
+    });
   }
 
 })();

0a0de3783eff18095b483d0866eee1274ea60b3b
berliner <berliner@dev030.com>
2021-12-15 21:36:30 +0100

    Add graphql endpoint to simply check the connection

---
+++ html/modules/custom/gho_graphql/src/Plugin/GraphQL/DataProducer/ConnectionStatus.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\gho_graphql\Plugin\GraphQL\DataProducer;
+
+use Drupal\graphql\Plugin\GraphQL\DataProducer\DataProducerPluginBase;
+
+/**
+ * Transforms a string to uppercase.
+ *
+ * @DataProducer(
+ *   id = "connection_status",
+ *   name = @Translation("Connection status"),
+ *   description = @Translation("Returns a string if the connection has been successful."),
+ *   produces = @ContextDefinition("string",
+ *     label = @Translation("Connection status")
+ *   )
+ * )
+ */
+class ConnectionStatus extends DataProducerPluginBase {
+
+  /**
+   * Value resolver.
+   *
+   * @return string
+   *   A status string.
+   */
+  public function resolve() {
+    return 'connected';
+  }
+
+}

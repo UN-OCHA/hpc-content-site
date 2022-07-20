acf637b2b59d7e783a492951fb9344726883ae6c
berliner <berliner@dev030.com>
2022-07-21 00:51:59 +0200

    Add paragraph behavior for promoted paragraphs

---
+++ html/modules/custom/gho_ncms/src/Plugin/paragraphs/Behavior/PromoteBehavior.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\gho_ncms\Plugin\paragraphs\Behavior;
+
+use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\paragraphs\ParagraphInterface;
+use Drupal\paragraphs\ParagraphsBehaviorBase;
+
+/**
+ * Provides a paragraphs behaviour for promoting paragraphs.
+ *
+ * @ParagraphsBehavior(
+ *   id = "promoted_behavior",
+ *   label = @Translation("Promotable paragraph"),
+ *   description = @Translation("Add option to mark a paragraph as promoted"),
+ * )
+ */
+class PromoteBehavior extends ParagraphsBehaviorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function view(array &$build, Paragraph $paragraph, EntityViewDisplayInterface $display, $view_mode) {
+    $promoted = $paragraph->getBehaviorSetting($this->getPluginId(), 'promoted', FALSE);
+    if ($promoted) {
+      $build['#attributes']['class'][] = 'paragraph-promoted';
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state) {
+    $form['promoted'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Promoted'),
+      '#default_value' => $paragraph->getBehaviorSetting($this->getPluginId(), 'promoted', FALSE),
+    ];
+    return $form;
+  }
+
+}

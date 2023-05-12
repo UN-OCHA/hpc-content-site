30488c95c27b85065255952595d92ee3cbd2955d
berliner <berliner@dev030.com>
2023-05-12 14:34:54 +0200

    Update to Drupal 10, fix tests

--- html/modules/custom/linked_responsive_image_media_formatter/tests/src/Functional/FieldFormatter/LinkedResponsiveImageMediaFormatterTest.php
+++ html/modules/custom/linked_responsive_image_media_formatter/tests/src/Functional/FieldFormatter/LinkedResponsiveImageMediaFormatterTest.php
@@ -43,7 +43,7 @@ class LinkedResponsiveImageMediaFormatterTest extends BrowserTestBase {
   /**
    * {@inheritdoc}
    */
-  protected $defaultTheme = 'classy';
+  protected $defaultTheme = 'starterkit_theme';
 
   /**
    * View display for the node type used in the tests.
@@ -76,7 +76,7 @@ class LinkedResponsiveImageMediaFormatterTest extends BrowserTestBase {
   /**
    * {@inheritdoc}
    */
-  protected function setUp() {
+  protected function setUp(): void {
     parent::setUp();
 
     // Create responsive image styles.

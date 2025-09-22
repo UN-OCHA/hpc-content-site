011e4390efb65581ac5c8e9dfe698d21c9ec87e9
berliner <berliner@users.noreply.github.com>
2025-09-22 15:11:22 +0200

    HPC-9919: Add preview of affected nodes to media delete confirm screen, add summary of updated nodes to status message after media deletion

--- html/modules/custom/ncms_ui/src/Form/ContentSoftDeleteForm.php
+++ html/modules/custom/ncms_ui/src/Form/ContentSoftDeleteForm.php
@@ -6,10 +6,14 @@ use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Ajax\CloseModalDialogCommand;
 use Drupal\Core\Form\ConfirmFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Render\Markup;
 use Drupal\Core\Url;
 use Drupal\ncms_ui\Ajax\ReloadPageCommand;
+use Drupal\ncms_ui\Entity\ContentInterface;
 use Drupal\ncms_ui\Entity\Media\MediaBase;
+use Drupal\ncms_ui\Entity\MediaInterface;
 use Drupal\ncms_ui\Traits\RouteMatchEntityTrait;
+use Drupal\node\NodeInterface;
 
 /**
  * Form class for content submit confirm forms.
@@ -33,13 +37,15 @@ class ContentSoftDeleteForm extends ConfirmFormBase {
     $t_args = [
       '@type' => strtolower($entity->getBundleLabel()),
     ];
-    if ($entity instanceof MediaBase) {
-      if (!$entity->hasMandatoryReferences()) {
-        return $this->t('This will remove this @type from public display and remove references to it from all the places (articles, documents or stories) where it is currently used. These references might not be restored if you untrash this item later. Are you sure?', $t_args);
-      }
-      else {
+    if ($entity instanceof MediaInterface) {
+      if ($entity->hasMandatoryReferences()) {
         $t_args['@places_used_url'] = $entity->toUrl('places-used')->toString();
         return $this->t('This @type is currently referred to in article paragraphs where it is mandatory. It cannot be moved to trash until those references are removed manually. Please refer to the <a href="@places_used_url" target="_blank">Places used</a> list.', $t_args);
+      }
+      elseif ($entity->hasOptionalReferences()) {
+        $usage_summary = $this->getUsageSummary($entity);
+        $t_args['@usage_summary'] = Markup::create(implode('', $usage_summary));
+        return $this->t('<p>This will remove this @type from public display and remove references to it from all the places (articles, documents or stories) where it is currently used. These references will not be restored if you untrash this item later. Are you sure?</p><p>Affected content:<br /><ul>@usage_summary</ul></p>', $t_args);
       }
     }
     return $this->t('This will remove this @type, including already published versions, from public display anywhere. Are you sure?', $t_args);
@@ -69,7 +75,7 @@ class ContentSoftDeleteForm extends ConfirmFormBase {
     // This is a special class to which JavaScript assigns dialog closing
     // behavior.
     $form['actions']['cancel']['#attributes']['class'][] = 'dialog-cancel';
-    if ($entity instanceof MediaBase && $entity->hasMandatoryReferences()) {
+    if ($entity instanceof MediaInterface && $entity->hasMandatoryReferences()) {
       $form['actions']['submit']['#access'] = FALSE;
     }
     return $form;
@@ -115,19 +121,65 @@ class ContentSoftDeleteForm extends ConfirmFormBase {
     if (!$entity) {
       throw new \Exception('Fatal error: Entity not found.');
     }
+
+    // Build the status message for this operation.
+    $message = $this->t('@type %title has been moved to the trash bin.', [
+      '@type' => $entity->getBundleLabel(),
+      '%title' => $entity->label(),
+    ]);
+
     if ($entity instanceof MediaBase && $entity->hasMandatoryReferences()) {
       throw new \Exception('Cannot delete entity with mandatory references.');
+    }
+    if ($entity instanceof MediaBase && $entity->hasOptionalReferences()) {
+      $usage_summary = $this->getUsageSummary($entity);
+      $message .= '<br>' . $this->t('These content entities have been update:<ul>@usage_summary</ul>', [
+        '@usage_summary' => Markup::create(implode('', $usage_summary)),
+      ]);
     }
     $entity->setDeleted();
     $entity->save();
 
     // And inform the user.
-    $this->messenger()->addStatus($this->t('@type %title has been moved to the trash bin.', [
-      '@type' => $entity->getBundleLabel(),
-      '%title' => $entity->label(),
-    ]));
+    $this->messenger()->addStatus(Markup::create($message));
 
     $form_state->setRedirectUrl($entity->getOverviewUrl());
+  }
+
+  /**
+   * Get a summary of the updates to be done.
+   *
+   * @param \Drupal\ncms_ui\Entity\MediaInterface $entity
+   *   The media entity being deleted.
+   *
+   * @return array
+   *   An array of strings containing markup.
+   */
+  private function getUsageSummary(MediaInterface $entity) {
+    $affected_nodes = $entity->getNodesAffectedByDeletion();
+    $usage_summary = array_map(function (NodeInterface $node) {
+      $tags = $node instanceof ContentInterface ? $node->getTags() : [];
+      $edit_link = $node->toLink('Edit', 'edit-form', [
+        'attributes' => ['target' => '_blank'],
+      ]);
+      $additional_info = [];
+      if (method_exists($node, 'getBundleLabel')) {
+        $additional_info[] = $node->getBundleLabel();
+      }
+      if (!empty($tags)) {
+        $additional_info[] = implode(', ', $tags);
+      }
+
+      $item = $node->label();
+      if (!empty($additional_info)) {
+        $item .= ' (' . implode(', ', $additional_info) . ')';
+      }
+      if ($edit_link->getUrl()->access()) {
+        $item .= ' - ' . $edit_link->toString();
+      }
+      return '<li>' . $item . '</li>';
+    }, $affected_nodes);
+    return $usage_summary;
   }
 
 }
